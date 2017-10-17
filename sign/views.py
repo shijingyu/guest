15a2f585cd6df9d4131788510186a57c0f2cc99d
shijingyu <945090896@qq.com>
2017-10-17 21:26:50 +0800

    cookie

--- sign/views.py
+++ sign/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,8 +14,17 @@ def login_action(request):
         username = request.POST.get('username','')
         password = request.POST.get('password','')
         if username == 'admin' and password == 'admin123':
-            return HttpResponse('login success!')
+            #return HttpResponse('login success!')
+            #return HttpResponseRedirect('/event_manage/')
+            respone = HttpResponseRedirect('/event_manage/')
+            respone.set_cookie('user',username,3600)
+            return respone
+
         else:
             return render(request,'index.html',{'error':'username or password error!'})
     else:
         return HttpResponse('login error!')
+
+def event_manage(request):
+    username = request.COOKIES.get('user','')
+    return render(request,'event_manage.html',{"user":username})

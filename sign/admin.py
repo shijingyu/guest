dd6684b00713feeb17cb3f69eb667e4da7ebbc69
shijingyu <945090896@qq.com>
2017-10-18 21:44:37 +0800

    添加model层和admin前台展现

--- sign/admin.py
+++ sign/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from sign.models import Event,Guest
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Guest)
